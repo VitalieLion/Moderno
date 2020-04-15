62c32bf0186f5fe8b2af748e22e7b1e190582df7
VitalieLion <vperdeleanu@gmail.com>
2020-04-15 11:17:38 +0300

    2-nd page

--- app/js/main.js
+++ app/js/main.js
@@ -15,7 +15,23 @@ $(function() {
     });
 
 
+   
+    
+
 
 
     var mixer = mixitup('.products__inner-box');
+});
+
+$(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix : "$"
+    
+});
+$('.icon-th-list').on('click', function(){
+    $('product__item').addClass('list')
 });
