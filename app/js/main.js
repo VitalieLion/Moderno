cd96226b09c78618d9a5a55da9af5a7ae8a7c5ad
VitalieLion <vperdeleanu@gmail.com>
2020-04-15 21:30:07 +0300

    add page 2-nd final

--- app/js/main.js
+++ app/js/main.js
@@ -16,7 +16,17 @@ $(function() {
 
 
    
-    
+    $('.icon-th-list').on('click', function(){
+        $('.product__item').addClass('list');
+        $('.icon-th-list').addClass ('active');
+        $('.icon-th-large').removeClass('active');
+    });
+
+    $('.icon-th-large').on('click', function(){
+        $('.product__item').removeClass('list');
+        $('.icon-th-large').addClass ('active');
+        $('.icon-th-list').removeClass ('active');
+    });
 
 
 
@@ -26,12 +36,11 @@ $(function() {
 $(".js-range-slider").ionRangeSlider({
     type: "double",
     min: 0,
-    max: 1000,
+    max: 600,
     from: 0,
     to: 600,
     prefix : "$"
     
 });
-$('.icon-th-list').on('click', function(){
-    $('product__item').addClass('list')
-});
+
+
